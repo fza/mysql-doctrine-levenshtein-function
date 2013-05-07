392e6f8eac01dffda56e7e69b3e4320877612cc6
Felix Zandanel <felix@zandanel.me>
2013-05-07 10:05:30 +0200

    Initial commit

---
+++ lib/Fza/MysqlDoctrineLevenshteinFunction/DQL/LevenshteinFunction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pixelart\CrmBundle\DQL;
+
+use Doctrine\ORM\Query\Lexer;
+
+class LevenshteinFunction
+{
+    public $firstStringExpression = null;
+    public $secondStringExpression = null;
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'levenshtein(' .
+            $this->firstStringExpression->dispatch($sqlWalker) . ', ' .
+            $this->secondStringExpression->dispatch($sqlWalker) .
+            ')';
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        // levenhstein(str1, str2)
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->firstStringExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->secondStringExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
